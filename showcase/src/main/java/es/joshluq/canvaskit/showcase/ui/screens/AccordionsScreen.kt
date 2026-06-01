361a79554c99a0523a5f6c89157a7c9e8bc67b4e
Josh Luque <josh.luque@gmail.com>
2026-06-01 13:16:59 +0200

    style: remove unused imports and clean up code in AccordionsScreen

--- showcase/src/main/java/es/joshluq/canvaskit/showcase/ui/screens/AccordionsScreen.kt
+++ showcase/src/main/java/es/joshluq/canvaskit/showcase/ui/screens/AccordionsScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -17,7 +16,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Palette
-import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +24,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import es.joshluq.canvaskit.components.buttons.CanvasKitIconButton
 import es.joshluq.canvaskit.components.inputs.CanvasKitSwitch
 import es.joshluq.canvaskit.components.layout.CanvasKitAccordion
